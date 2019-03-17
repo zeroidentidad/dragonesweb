f84fcae0780167fcc3b31ffa36a9e898bde5e768
Jesus Antonio Ferrer Sanchez <jafs-2050@hotmail.com>
2019-03-17 17:53:17 -0600

    def rasgos
    codigo inicial para la generacion de rasgos del dragon

--- backend/dragon.js
+++ backend/dragon.js
@@ -1,12 +1,28 @@
+const RASGOS = require('./rasgos.json');
+
 const DEFAULT_PROPIEDADES = {
 	nickname: 'sinnombre',
-	get nacimientofecha(){ return new Date() }
+	get nacimientofecha(){ return new Date() },
+	get rasgosAleatorios(){
+		const rasgos = [];
+		RASGOS.forEach(RASGO => {
+			const tipoRasgo = RASGO.tipo;
+			const valoresRasgo = RASGO.values;
+
+			const valorRasgo = valoresRasgo[
+				Math.floor(Math.random()*valoresRasgo.length)
+			];
+			rasgos.push({tipoRasgo, valorRasgo});
+		});
+	 return rasgos;
+	}
 }
 
 class Dragon{
-	constructor({ nacimientofecha, nickname } = {}){
+	constructor({ nacimientofecha, nickname, rasgos } = {}){
 		this.nacimientofecha = nacimientofecha || DEFAULT_PROPIEDADES.nacimientofecha;
 		this.nickname = nickname || DEFAULT_PROPIEDADES.nickname;
+		this.rasgos = rasgos || DEFAULT_PROPIEDADES.rasgosAleatorios;
 	}
 }
 
